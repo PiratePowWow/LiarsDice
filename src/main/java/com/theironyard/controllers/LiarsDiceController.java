4f25e06a22a3b274ddceb4b1c3a6458c6666399b
PiratePowWow <Thi3v3ry>
2016-04-12 13:46:55 -0400

    Started Building Routes

--- src/main/java/com/theironyard/controllers/LiarsDiceController.java
+++ src/main/java/com/theironyard/controllers/LiarsDiceController.java
@@ -1,14 +1,21 @@
 package com.theironyard.controllers;
 
+import com.theironyard.dtos.PlayerDto;
+import com.theironyard.entities.GameState;
 import com.theironyard.entities.Player;
 import com.theironyard.services.GameStateRepository;
 import com.theironyard.services.PlayerRepository;
 import com.theironyard.utils.GameLogic;
 import org.h2.tools.Server;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
@@ -18,7 +25,7 @@ import java.util.ArrayList;
 /**
  * Created by PiratePowWow on 4/5/16.
  */
-@RestController
+@Controller
 public class LiarsDiceController {
     @Autowired
     PlayerRepository players;
@@ -31,10 +38,71 @@ public class LiarsDiceController {
         Server.createWebServer().start();
     }
 
-    @MessageMapping("/lobby/{roomCode}")
-    public ArrayList<Player> scoreboard(@DestinationVariable String roomCode){
-        return new ArrayList<Player>(players.findByGameStateOrderBySeatNum(gameStates.findOne(roomCode)));
+//    @MessageMapping("/lobby/")
+//    public ArrayList<Player> scoreboard(@DestinationVariable String roomCode){
+//        return new ArrayList<Player>(players.findByGameStateOrderBySeatNum(gameStates.findOne(roomCode)));
+//    }
+
+    @MessageMapping("/lobby/rollDice")
+    @SendTo("/topic/playerList")
+    public ArrayList<Player> rollDice(String id) {
+        Player playerRollingDice = players.findOne(id);
+        GameState gameState = playerRollingDice.getGameState();
+        if(playerRollingDice.getDice() == null){
+            playerRollingDice.setDice(gameLogic.rollDice());
+            players.save(playerRollingDice);
+        }
+        if (gameLogic.allDiceRolled(gameState.getRoomCode())){
+            gameLogic.setNextActivePlayer(gameState.getRoomCode());
+        }
+        return players.findByGameStateOrderBySeatNum(gameState);
     }
+
+    @MessageMapping("/lobby/setStake")
+    @SendTo("/topic/playerList")
+    public ArrayList<Player> setStake(String id, ArrayList<Integer> dice) {
+        Player playerSettingStake = players.findOne(id);
+        GameState gameState = playerSettingStake.getGameState();
+        if(gameLogic.isActivePlayer(id) && gameLogic.isValidRaise(gameState, dice)){
+            playerSettingStake.setStake(dice);
+            players.save(playerSettingStake);
+            gameLogic.setNextActivePlayer(gameState.getRoomCode());
+        }
+        return players.findByGameStateOrderBySeatNum(playerSettingStake.getGameState());
+    }
+
+    @MessageMapping("/lobby/callBluff")
+    @SendTo("/topic/loser")
+    public Player callBluff(String id) {
+        Player playerCallingBluff = players.findOne(id);
+        GameState gameState = playerCallingBluff.getGameState();
+        if(gameLogic.isActivePlayer(id)){
+            Player loser = gameLogic.determineLoser(gameState);
+            return loser;
+        }
+        return playerCallingBluff;
+    }
+
+    @MessageMapping("/lobby/resetGame")
+    @SendTo("/topic/playerList")
+    public ArrayList<Player> resetGame(String id){
+        Player playerRequestingReset = players.findOne(id);
+        GameState gameState = playerRequestingReset.getGameState();
+        gameLogic.resetGameState(gameState.getRoomCode());
+        GameState newGame = playerRequestingReset.getGameState();
+        gameLogic.setNextActivePlayer(newGame.getRoomCode());
+        return players.findByGameStateOrderBySeatNum(newGame);
+    }
+
+
+
+
+//    @RequestMapping(path = "/newGame", method = RequestMethod.POST)
+//    public ResponseEntity<Object> newGame(String name){
+//        return new ResponseEntity<Object>(name, HttpStatus.OK);
+//    }
+
+
 
 
 //    @MessageMapping("/hello")
--- src/main/java/com/theironyard/dtos/PlayerDto.java
+++ src/main/java/com/theironyard/dtos/PlayerDto.java
@@ -6,30 +6,36 @@ import java.util.ArrayList;
  * Created by PiratePowWow on 4/5/16.
  */
 public class PlayerDto {
+    private String id;
     private String name;
+    private String roomCode;
     private ArrayList<Integer> dice;
+    private ArrayList<Integer> stake;
     private int seatNum;
 
-    public PlayerDto(String name, ArrayList<Integer> dice, int seatNum) {
+    public PlayerDto(String id, String name, String roomCode, ArrayList<Integer> dice, ArrayList<Integer> stake, int seatNum) {
+        this.id = id;
         this.name = name;
+        this.roomCode = roomCode;
         this.dice = dice;
+        this.stake = stake;
         this.seatNum = seatNum;
     }
 
-    public PlayerDto(String name, ArrayList<Integer> dice) {
+    public PlayerDto(String id, String name, String roomCode, ArrayList<Integer> stake, int seatNum) {
+        this.id = id;
         this.name = name;
-        this.dice = dice;
-    }
-
-    public PlayerDto() {
-    }
-
-    public int getSeatNum() {
-        return seatNum;
-    }
-
-    public void setSeatNum(int seatNum) {
+        this.roomCode = roomCode;
+        this.stake = stake;
         this.seatNum = seatNum;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -40,11 +46,35 @@ public class PlayerDto {
         this.name = name;
     }
 
+    public String getRoomCode() {
+        return roomCode;
+    }
+
+    public void setRoomCode(String roomCode) {
+        this.roomCode = roomCode;
+    }
+
     public ArrayList<Integer> getDice() {
         return dice;
     }
 
     public void setDice(ArrayList<Integer> dice) {
         this.dice = dice;
+    }
+
+    public ArrayList<Integer> getStake() {
+        return stake;
+    }
+
+    public void setStake(ArrayList<Integer> stake) {
+        this.stake = stake;
+    }
+
+    public int getSeatNum() {
+        return seatNum;
+    }
+
+    public void setSeatNum(int seatNum) {
+        this.seatNum = seatNum;
     }
 }
