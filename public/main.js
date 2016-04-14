806e84e6a58f5d8cf31778a660774ef9c5e548db
Elizabeth Evans <ickabeth@gmail.com>
2016-04-14 12:28:35 -0400

    dice are working and accepting random numbers

--- public/main.js
+++ public/main.js
@@ -24,7 +24,13 @@ var liarsDice = {
       console.log("you clicked submit");
 
       var name = $('input[name="name"]').val();
-      var roomCode = $('input[roomCode="roomCode"]').val();
+      var roomCode
+      if ($('input[roomCode="roomCode"]').val() === "") {
+        roomCode = "undefined";
+      } else {
+        roomCode = $('input[roomCode="roomCode"]').val()
+      };
+
       // socket stuff
       $('.lobby').removeClass('inactive');
       $('.homePage').addClass('inactive');
@@ -33,14 +39,12 @@ var liarsDice = {
     });
     $('.box').on('click', function(event){
       socket.playRollDie();
-
-      // var onLobby = function(data){
-      //   queryParam = data.dice[0];
-      //   queryParam2 = data.dice[1];
-      //   queryParam3 = data.dice[2];
-      //   queryParam4 = data.dice[3];
-      //   queryParam5 = data.dice[4];
-      // };
+      window.rollDie = function() {
+        socket.playRollDie();
+      }
+      setTimeout(function() {
+        rollDicePage(window.diceToDisplay)
+      },2000);
 
       event.preventDefault();
       // onLobby();
@@ -57,13 +61,7 @@ var liarsDice = {
     //   _.each()
     //   $('.nameContent').html("");
     // };
-    // var onLobby = function(data){
-    //   queryParam = data.dice[0];
-    //   queryParam2 = data.dice[1];
-    //   queryParam3 = data.dice[2];
-    //   queryParam4 = data.dice[3];
-    //   queryParam5 = data.dice[4];
-    // };
+
     // var onLoser = function(){
     //   $('.bluff').on('click', function(event){
     //   console.log("you clicked the bluff button");
@@ -89,95 +87,9 @@ var liarsDice = {
     });
     //  lobby
   },
-  styling: function() {
+  styling: function(diceObject) {
 
-// query params= the number the dice lands on
-// spinCount=how many times the dice spins before it lands on the number
-// dice one
-    var faceOne = 1;
-    var spinCount = 0;
-    var currentSpinCount = 0;
-    var showOne = function() {
-      $('#cube').attr('class', 'showOne' + faceOne);
-      if (faceOne == 6) {
-        faceOne = 1;
-      } else {
-        faceOne++;
-      }
-      if (currentSpinCount == spinCount) {
-        return faceOne = queryParam;
-      }
-      currentSpinCount++;
-    };
-    var timer1 = setInterval(showOne, 500);
-    // dice two
-    var faceTwo = 1;
-    var spinCount2 = 2;
-    var currentSpinCount2 = 0;
-    var showTwo = function() {
-      $('#cube2').attr('class', 'showTwo' + faceTwo);
-      if (faceTwo == 6) {
-        faceTwo = 1;
-      } else {
-        faceTwo++;
-      }
-      if (currentSpinCount2 == spinCount2) {
-        return faceTwo = queryParam2;
-      }
-      currentSpinCount2++;
-    };
-    var timer2 = setInterval(showTwo, 500);
-    // third dice
-    var faceThree = 1;
-    var spinCount3 = 2;
-    var currentSpinCount3 = 0;
-    var showThree = function() {
-      $('#cube3').attr('class', 'showThree' + faceThree);
-      if (faceThree == 6) {
-        faceThree = 1;
-      } else {
-        faceThree++;
-      }
-      if (currentSpinCount3 == spinCount3) {
-        return faceThree = queryParam3;
-      }
-      currentSpinCount3++;
-    };
-    var timer3 = setInterval(showThree, 500);
-    // fourth dice
-    var faceFour = 1;
-    var spinCount4 = 1;
-    var currentSpinCount4 = 0;
-    var showFour = function() {
-      $('#cube4').attr('class', 'showFour' + faceFour);
-      if (faceFour == 6) {
-        faceFour = 1;
-      } else {
-        faceFour++;
-      }
-      if (currentSpinCount4 == spinCount4) {
-        return faceFour = queryParam4;
-      }
-      currentSpinCount4++;
-    };
-    var timer4 = setInterval(showFour, 500);
-    // fifth dice
-    var faceFive = 1;
-    var spinCount5 = 2;
-    var currentSpinCount5 = 0;
-    var showFive = function() {
-      $('#cube5').attr('class', 'showFive' + faceFive);
-      if (faceFive == 6) {
-        faceFive = 1;
-      } else {
-        faceFive++;
-      }
-      if (currentSpinCount5 == spinCount5) {
-        return faceFive = queryParam5;
-      }
-      currentSpinCount5++;
-    };
-    var timer5 = setInterval(showFive, 500);
+//
   }
 
 }
@@ -190,10 +102,10 @@ function Socket() {
   var _this = this;
   var socketInternal;
 
-  _this.connectSocket = function(name) {
+  _this.connectSocket = function(name,roomCode) {
     var ws = new SockJS("/liarsDice")
     socketInternal = Stomp.over(ws)
-    socketInternal.connect({name:name, roomCode: "undefined"}, function() {
+    socketInternal.connect({name:name, roomCode: roomCode}, function() {
       _this.onSocketConnected()
     })
   };
@@ -210,35 +122,46 @@ function Socket() {
   // PURPOSE - Signal to the server the player wants to reset the game(start a new game, but keep same roomCode and existing players)
 
       socketInternal.subscribe("/topic/lobby/" + playerId, getDiceBack)
-      socketInternal.subscribe("/topic/playerList", getDiceBack);
-      socketInternal.subscribe("/topic/loser", getDiceBack)
+      socketInternal.subscribe("/topic/playerList", test2);
+      socketInternal.subscribe("/topic/loser", test3)
   // PURPOSE - player's dice)
 
       // socketInternal.subscribe("/topic/lobby/" + sessionId, onLobby);
 
+      function test1(data) {
+        console.log("SUBSCRIBE TOPIC LOBBY", data);
+
+      }
+      function test2(data) {
+        console.log("SUBSCRIBE TOPIC playerlist",data);
+
+      }  function test3(data) {
+          console.log("SUBSCRIBE TOPIC LOSER",data);
+
+        }
 
   // @return loserDto - this is the PlayerDto of the losing player
       // socketInternal.subscribe("/topic/loser", getDiceBack);
       socketInternal.send("/app/lobby/" + playerId, {}, "");
       socketInternal.send("/app/lobby/JoinGame", {}, playerId);
       socketInternal.send("/app/lobby/resetGame", {}, playerId);
-      var millisecondsToWait = 200;
-            setTimeout(function() {
-                socketInternal.send("/app/lobby/rollDice", {}, playerId);
-      }, millisecondsToWait);
-      var millisecondsToWait = 200;
-      setTimeout(function() {
-          socketInternal.send("/app/lobby/" + playerId, {}, "");
-      }, millisecondsToWait);
-      //socketInternal.send("/app/lobby/resetGame", {}, playerId);
-      //_this.sendFirstConnection(playerId);
+      // var millisecondsToWait = 200;
+      //       setTimeout(function() {
+      //           socketInternal.send("/app/lobby/rollDice", {}, playerId);
+      // }, millisecondsToWait);
+      // var millisecondsToWait = 200;
+      // setTimeout(function() {
+      //     socketInternal.send("/app/lobby/" + playerId, {}, "");
+      // }, millisecondsToWait);
+      // socketInternal.send("/app/lobby/resetGame", {}, playerId);
+      // _this.sendFirstConnection(playerId);
 
     };
 
 
     _this.playRollDie = function() {
       console.log("THIS IS A PLAYER ID IN ROLLDIE", playerId);
-      socketInternal.send("/app/lobby/rollDice", playerId);
+      socketInternal.send("/app/lobby/rollDice", {}, playerId);
     }
   // connectSocket();
     _this.sendFirstConnection = function(thingId) {
@@ -246,6 +169,113 @@ function Socket() {
     }
 
     function getDiceBack(data) {
+      window.preStuff = data;
       console.log("WE GOT STUFF BACK", data);
+      data = JSON.parse(data.body);
+      window.glob = data;
+      // console.log("SHOW DATA DICE", data.dice);
+      if(data.dice) {
+        var diceRol = {
+            queryParam: data.dice[0],
+            queryParam2: data.dice[1],
+            queryParam3: data.dice[2],
+            queryParam4: data.dice[3],
+            queryParam5: data.dice[4],
+          };
+          console.log("HEEERERERERE", diceRol);
+          window.diceToDisplay = diceRol
+          return diceRol
+      }
     }
+}
+
+
+
+function rollDicePage(diceObject) {
+  // spinCount=how many times the dice spins before it lands on the number
+  // dice one
+      var faceOne = 1;
+      var spinCount = 0;
+      var currentSpinCount = 0;
+      var showOne = function() {
+        $('#cube').attr('class', 'showOne' + faceOne);
+        if (faceOne == 6) {
+          faceOne = 1;
+        } else {
+          faceOne++;
+        }
+        if (currentSpinCount == spinCount) {
+          return faceOne = diceObject.queryParam;
+        }
+        currentSpinCount++;
+      };
+      var timer1 = setInterval(showOne, 500);
+      // dice two
+      var faceTwo = 1;
+      var spinCount2 = 2;
+      var currentSpinCount2 = 0;
+      var showTwo = function() {
+        $('#cube2').attr('class', 'showTwo' + faceTwo);
+        if (faceTwo == 6) {
+          faceTwo = 1;
+        } else {
+          faceTwo++;
+        }
+        if (currentSpinCount2 == spinCount2) {
+          return faceTwo = diceObject.queryParam2;
+        }
+        currentSpinCount2++;
+      };
+      var timer2 = setInterval(showTwo, 500);
+      // third dice
+      var faceThree = 1;
+      var spinCount3 = 2;
+      var currentSpinCount3 = 0;
+      var showThree = function() {
+        $('#cube3').attr('class', 'showThree' + faceThree);
+        if (faceThree == 6) {
+          faceThree = 1;
+        } else {
+          faceThree++;
+        }
+        if (currentSpinCount3 == spinCount3) {
+          return faceThree = diceObject.queryParam3;
+        }
+        currentSpinCount3++;
+      };
+      var timer3 = setInterval(showThree, 500);
+      // fourth dice
+      var faceFour = 1;
+      var spinCount4 = 1;
+      var currentSpinCount4 = 0;
+      var showFour = function() {
+        $('#cube4').attr('class', 'showFour' + faceFour);
+        if (faceFour == 6) {
+          faceFour = 1;
+        } else {
+          faceFour++;
+        }
+        if (currentSpinCount4 == spinCount4) {
+          return faceFour = diceObject.queryParam4;
+        }
+        currentSpinCount4++;
+      };
+      var timer4 = setInterval(showFour, 500);
+      // fifth dice
+      var faceFive = 1;
+      var spinCount5 = 2;
+      var currentSpinCount5 = 0;
+      var showFive = function() {
+        $('#cube5').attr('class', 'showFive' + faceFive);
+        if (faceFive == 6) {
+          faceFive = 1;
+        } else {
+          faceFive++;
+        }
+        if (currentSpinCount5 == spinCount5) {
+          return faceFive = diceObject.queryParam5;
+        }
+        currentSpinCount5++;
+      };
+      var timer5 = setInterval(showFive, 500);
 }
