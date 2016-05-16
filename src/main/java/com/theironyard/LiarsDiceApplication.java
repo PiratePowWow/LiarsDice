0d762f2aa95c9ef53adaec5310b0c745260f25b9
PiratePowWow <Thi3v3ry>
2016-05-16 10:03:32 -0500

    War File - latest styles

--- src/main/java/com/theironyard/LiarsDiceApplication.java
+++ src/main/java/com/theironyard/LiarsDiceApplication.java
@@ -2,9 +2,16 @@ package com.theironyard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LiarsDiceApplication {
+public class LiarsDiceApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(LiarsDiceApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiarsDiceApplication.class, args);
---
+++ upstartLiarsDice.sh
@@ -0,0 +1,35 @@
+echo '***************************************************'
+echo '**** starting Liars-Dice deploy... ****'
+echo '***************************************************'
+
+echo '**** stopping running service ****'
+
+sudo initctl stop musc-smoking
+
+echo '**** pulling changes ****'
+
+cd ~/build/musc-smoking-cessation-api
+git pull origin dev
+
+echo '**** echo creating zip archive ****'
+
+./activator -mem 512 dist
+
+echo '**** unzipping archive... ****'
+
+cd ~/dist
+sudo rm -rf musc-smoking-cessation-api-1.0-SNAPSHOT/
+sudo unzip ~/build/musc-smoking-cessation-api/target/universal/musc-smoking-cessation-api-1.0-SNAPSHOT.zip
+
+echo '**** starting service ****'
+
+sudo initctl start musc-smoking
+
+echo '****************************************************'
+echo '**** riskband musc-smoking-cessation SUCCESS... ****'
+echo '****************************************************'
+pull changes from git
+build war
+run war
+then you can just run something like
+sudo ssh -i ~/.ssh/musc-smoking-cessation-dev.pem ec2-user@ec2-52-73-20-23.compute-1.amazonaws.com '~/deploy.sh'
